8b65cda8498558d7e246214495eee5328a669c02
www <l2u0k0a0s.ld@gmail.com>
2019-11-17 20:46:29 +0100

    Update check_txtInput.py

--- check_txtInput.py
+++ check_txtInput.py
@@ -1,16 +1,18 @@
-import time
 import sys
 import os
 from selenium import webdriver
 
 # Globals
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches",["ignore-certificate-errors"])
+options.add_argument('headless')
+options.add_argument('window-size=0x0')
 path = os.path.dirname(os.path.abspath(__file__))
 print(path)
-driver = webdriver.Chrome(path + '/chromedriver.exe')
+driver = webdriver.Chrome(path + '/chromedriver.exe', options=options)
 base_url = 'https://beta.esportal.se/profile/'
 url_path = path + '/list.txt'
 id = ''
-
 
 # Opens url
 def scan(id):
@@ -32,6 +34,7 @@ with open(url_path, 'r+') as fp:
         if driver.title == 'Home - Esportal':
             print('free: {0}'.format(id.strip('\n')))
             write_file(id)
+
 
 # Exit when complete
 fp.close()
